67f6b1e4996fb81a303793bcb7fdcede5b119fc0
madifrontend <madifrontend@gmail.com>
2024-01-16 10:06:37 +0330

    add contact-us

---
+++ src/contact-us/ContactUs.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+function ContactUs() {
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    reset,
+    formState: { errors, isSubmitting },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {/* register your input into the hook by invoking the "register" function */}
+        <input defaultValue="test" {...register("example")} />
+
+        {/* include validation with required or other standard HTML validation rules */}
+        <input {...register("exampleRequired", { required: true })} />
+        {/* errors will return when field validation fails  */}
+        {errors.exampleRequired && <span>This field is required</span>}
+
+        <input type="submit" />
+      </form>
+    </div>
+  );
+}
+
+export default ContactUs;

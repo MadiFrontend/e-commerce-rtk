2cd43a7545af3a86b6869bea16111ff22ee036ed
madifrontend <madifrontend@gmail.com>
2024-01-16 10:55:59 +0330

    update tailwind file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        lg: "3rem",
+        xl: "4rem",
+      },
+    },
     extend: {
       colors: {
         primery: "#ff0000",

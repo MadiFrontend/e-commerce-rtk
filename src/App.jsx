7c0a0772562655218db6700e3536be3bc9d6a231
madifrontend <madifrontend@gmail.com>
2024-01-16 10:05:57 +0330

    update route

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,12 @@
 import "./App.css";
-import Cards from "./card/Cards";
-import { Header } from "./header/Header";
-import Slider from "./heroSection/Slider";
-// import { Provider, useDispatch, useSelector } from "react-redux";
-import { Route, Routes } from "react-router-dom";
-import Cartpage from "./cartPage/Cartpage";
-import Finalpage from "./finalPage/Finalpage";
-import Detailpage from "./detailpage/Detailpage";
-// import { useEffect } from "react";
-// import { updateTotal } from "./redux/features/productSlice/productSlice";
+import router from "./router";
+import { RouterProvider } from "react-router-dom";
 
 function App() {
-  /* Component */
-
   return (
-    <div className="flex flex-col items-center justify-center text-[#222]   ">
-      <Header />
-      <div className=" text-myFont w-[85%] overflow-x-hidden ">
-        <Routes>
-          <Route path="/" element={<Slider />} />
-          <Route path="/products" element={<Cards />} />
-          <Route path="/cartpage" element={<Cartpage />} />
-          <Route path="/finalpage" element={<Finalpage />} />
-          <Route path="products/:productId" element={<Detailpage />} />
-          {/* <Route path="/newproducts" element={<Newproducts />} /> */}
-        </Routes>
-      </div>
-    </div>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
 }
 

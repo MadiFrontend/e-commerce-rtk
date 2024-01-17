393d5bbc33df6aa9c60a425ef0b639387a0d9c5b
madifrontend <madifrontend@gmail.com>
2024-01-17 17:19:51 +0330

    Update contactUs ui

--- src/pages/contact-us/ContactUs.jsx
+++ src/pages/contact-us/ContactUs.jsx
@@ -16,47 +16,65 @@ function ContactUs() {
 
   return (
     <>
-      <h1>
-        Love to hear from you
+      <h1 className="font-bold text-[2.5rem]">
+        Love to hear from you,
         <br />
         Get in touch 👋
       </h1>
 
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className="flex flex-col items-center gap-5 "
+        className="flex  flex-col items-center gap-5 pt-8"
       >
-        {/* register your input into the hook by invoking the "register" function */}
-        <label htmlFor="test1">Name</label>
-        <input
-          id="test1"
-          {...register("username", { required: "username is required" })}
-          placeholder="Enter your Name ..."
-          className="border-2 rounded-lg px-2 py-1"
-        />
-        {errors.username && (
-          <p className="text-red-400">{errors.username.message}</p>
-        )}
+        <div className="flex justify-between w-[60%] ">
+          <div className="flex flex-col gap-2  w-[45%]">
+            <label htmlFor="test1">Name:</label>
+            <input
+              id="test1"
+              {...register("username", { required: "Name is required" })}
+              placeholder="Enter your Name ..."
+              className="bg-slate-100 w-full p-2 "
+            />
+            {errors.username && (
+              <p className="text-red-400">{errors.username.message}</p>
+            )}
+          </div>
 
-        {/* include validation with required or other standard HTML validation rules */}
-        <label htmlFor="test1">Password</label>
-        <input
-          {...register("password", {
-            required: "password is required",
-            minLength: {
-              value: 10,
-              message: "Password must be at least 10 charchters",
-            },
-          })}
-          placeholder="Enter your Password ..."
-          className="border-2 rounded-lg px-2 py-1"
-        />
-        {/* errors will return when field validation fails  */}
-        {errors.password && (
-          <p className="text-red-400">{errors.password.message}</p>
-        )}
+          <div className="flex  flex-col gap-2  w-[45%] ">
+            <label htmlFor="test1">Email:</label>
+            <input
+              {...register("email", {
+                required: "Don't keep us hanging, we need your email!",
+                pattern: {
+                  value: /^\S+@\S+$/i,
+                  message: "Oops! Please enter a valid email address.",
+                },
+              })}
+              placeholder="Enter your Email ..."
+              className="bg-slate-100 w-full p-2 "
+              type="email"
+            />
+            {errors.email && (
+              <span className="text-red-400">{errors.email.message}</span>
+            )}
+          </div>
+        </div>
+        <div className="flex justify-between w-[60%] ">
+          <div className="flex flex-col gap-2 w-full ">
+            <label htmlFor="message">Your message:</label>
+            <textarea
+              id="message"
+              {...register("message", { required: "Your message required" })}
+              placeholder="Tell us whatever you want ..."
+              className="bg-slate-100 w-full px-5 py-1 "
+            />
+            {errors.username && (
+              <p className="text-red-400">{errors.message.message}</p>
+            )}
+          </div>
+        </div>
 
-        <input
+        {/* <input
           {...register("confirmPassword", {
             required: "confirmPassword is required",
             validate: (value) =>
@@ -65,15 +83,15 @@ function ContactUs() {
           placeholder="confirm your Password ..."
           className="border-2 rounded-lg px-2 py-1"
         />
-        {/* errors will return when field validation fails  */}
+       
         {errors.confirmPassword && (
           <p className="text-red-400">{errors.confirmPassword.message}</p>
-        )}
+        )} */}
 
         <input
           type="submit"
           disabled={isSubmitting}
-          className="rounded-lg px-4 py-1 bg-orange-500"
+          className=" w-[60%] py-1 bg-orange-500 text-white disabled:bg-slate-800"
         />
       </form>
     </>

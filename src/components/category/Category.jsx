83a711040f1e4658fb5cb34668816923b17a5176
madifrontend <madifrontend@gmail.com>
2024-02-10 14:13:26 +0330

    add mega menu

---
+++ src/components/category/Category.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { BiCategory } from "react-icons/bi";
+
+const categoryItemsList = [
+  {
+    title: "SHOE & BAG",
+    list: ["Casual shoes", "boots", "sandals", "slippers"],
+  },
+  {
+    title: "SHOE & BAG",
+    list: ["Casual shoes", "boots", "sandals", "slippers"],
+  },
+  {
+    title: "SHOE & BAG",
+    list: ["Casual shoes", "boots", "sandals", "slippers"],
+  },
+  {
+    title: "SHOE & BAG",
+    list: ["Casual shoes", "boots", "sandals", "slippers"],
+  },
+  {
+    title: "SHOE & BAG",
+    list: ["Casual shoes", "boots", "sandals", "slippers"],
+  },
+  {
+    title: "SHOE & BAG",
+    list: ["Casual shoes", "boots", "sandals", "slippers"],
+  },
+];
+
+function Category() {
+  return (
+    <div className="group py-7 hover:text-primery">
+      <button className="flex items-center gap-1">
+        <BiCategory />
+        <p className="font-semibold text-lg ">Categories</p>
+      </button>
+
+      <div className="w-[85%] h-auto top-[220px] bg-white rounded-md absolute left-1/2 transform -translate-x-1/2 hidden -translate-y-1/2 group-hover:grid grid-cols-[2fr_1fr] px-5 items-center">
+        <div className="grid grid-cols-3 gap-10 py-4">
+          {categoryItemsList.map((item) => (
+            <ul>
+              <p className=" text-black font-semibold ">{item.title}</p>
+              {item.list.map((i) => {
+                return (
+                  <li className="text-gray-500 text-sm hover:text-primery cursor-pointer">
+                    {i}
+                  </li>
+                );
+              })}
+            </ul>
+          ))}
+        </div>
+
+        <img
+          src="/public/images/lady-menu.png"
+          alt="lady"
+          className="ml-auto w-[200px]"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Category;

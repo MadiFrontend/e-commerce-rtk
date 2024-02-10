fedf19a8e7aa52a78c03b068e5f0d25dca21d6c4
madifrontend <madifrontend@gmail.com>
2024-02-10 17:38:03 +0330

    update cart-modal style

--- src/components/cartmodal/Cartmodal.jsx
+++ src/components/cartmodal/Cartmodal.jsx
@@ -13,39 +13,43 @@ const Cartmodal = ({ cart, shown, close }) => {
         {" "}
       </div>
       <div
-        className="bg-white w-[22%] h-[450px]  shadow-2xl border rounded-2xl absolute z-10 right-[15px] top-[80px] overflow-y-scroll scrollbar-hide "
+        className="bg-white w-[20%] h-[350px]  shadow-2xl border rounded-md absolute z-10 right-[100px] top-[80px] overflow-y-scroll scrollbar-hide "
         onClick={(e) => {
           e.stopPropagation();
         }}
       >
         {cart.length === 0 ? (
-          <div className="h-full flex justify-center items-center text-2xl text-black">
+          <div className="h-full flex justify-center items-center text-2xl text-black  ">
             <h2>Your Cart Is Empty!</h2>
           </div>
         ) : (
           cart.slice(0, 3).map((search) => {
             return (
               <div
-                className="flex items-center my-3 hover:shadow-sm pb-1 cursor-pointer pr-2"
+                className="flex items-center py-3 cursor-pointer pr-2 border-b-[1px] border-gray-200 mx-5"
                 key={search.id}
               >
-                <div className="w-[90px] h-[70px] mx-5">
+                <div>
                   <Link to={`/products/${search.id}`}>
-                    <img src={search.image} alt="" className="w-full h-full" />
+                    <img
+                      src={search.image}
+                      alt=""
+                      className="object-contain w-[50px] h-[50px]"
+                    />
                   </Link>
                 </div>
-                <div className=" w-[80%] flex flex-col gap-4">
-                  <p className="text-black">
-                    {search.title.substring(0, 35)}...
+                <div className=" w-[80%] flex flex-col gap-2  ml-3">
+                  <p className="text-black text-sm">
+                    {search.title.substring(0, 25)}...
                   </p>
-                  <div className="flex items-center gap-4">
-                    <p className="text-primery font-bold mt-1 ">
+                  <div className="flex items-center justify-between">
+                    <p className="text-gray-500 font-semibold ">
                       ${search.price}
                     </p>
                     <BsFillTrashFill
                       color="red"
                       size={18}
-                      className="cursor-pointer mt-1"
+                      className="cursor-pointer "
                       onClick={() => dispatch(removeItem(search.id))}
                     />
                   </div>
@@ -57,8 +61,8 @@ const Cartmodal = ({ cart, shown, close }) => {
         {cart.length !== 0 ? (
           <div className="flex justify-center items-center pb-5">
             <Link to="/cartpage">
-              <button className="border border-primery text-primery px-4 py-1 rounded-2xl mt-3 hover:bg-primery hover:text-white">
-                All Cart Items
+              <button className="border bg-primery px-4 py-1 rounded-md mt-2">
+                View All Items
               </button>
             </Link>
           </div>
